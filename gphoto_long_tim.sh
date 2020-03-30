a5c507fee57c33e5bb98ffad4e3d3b72912bca76
Silvan <siliwalker@gmail.com>
2020-03-30 20:18:40 +0200

    Re Write to match new Sunwait Commandes

--- gphoto_long_tim.sh
+++ gphoto_long_tim.sh
@@ -1,34 +1,24 @@
-#!/bin/bash                                                          
-#                                                                    
+#!/bin/bash
+#
 # ------------------------------------------------------------------$
-# This script capable to executed from cron, capturing camera image, 
-# copying localy and uploading that to a given FTP server            
+# This script capable to executed from cron, capturing camera image,
+# modifing the exposure on Sunrise and Sunset Time
 # ------------------------------------------------------------------$
-#                                                                    
-# HUN: Idokep webkamera FTP feltoltes automatizalo script RaspberryPi
-# szamitogephez, kozonseges fenykepezogep vezerlesere (Canon S5 IS   
-# geppel tesztelve)                                                  
-#                                                                    
-# Before run install: sudo apt-get install gphoto2 usbmount          
-                                                                     
-SUNRISE=`sunwait -p 47N 9E 2>&1 | grep 'Sun rises' | cut -d' ' -f 22`
-SUNRISE=`echo $SUNRISE | cut -b1-2`:`echo $SUNRISE | cut -b3-4`:00   
-SUNSET=`sunwait -p 47N 9E 2>&1 | grep 'Sun rises' | cut -d' ' -f 25` 
-SUNSET=`echo $SUNSET | cut -b1-2`:`echo $SUNSET | cut -b3-4`:00      
-echo $SUNRISE                                                        
-echo $SUNSET                                                         
-                                                                     
-DATENOW="$(date +%H:%M:%S)"                                          
-echo $DATENOW                                                        
-#if [[ "$DATENOW" < "17:40:00" && "$DATENOW" > "06:30:00" ]]; then   
-if [[ "$DATENOW" < "$SUNSET" && "$DATENOW" > "$SUNRISE" ]]; then     
-                                                                     
-    gphoto2 --set-config /main/capturesettings/exposurecompensation=$
-                                                                     
-else                                                                 
-    gphoto2 --set-config /main/capturesettings/exposurecompensation=$
-                                                                     
-fi                                                                   
-                          [ Read 33 lines ]                          
-^G Get Help  ^O Write Out ^W Where Is  ^K Cut Text  ^J Justify       
-^X Exit      ^R Read File ^\ Replace   ^U Uncut Text^T To Linter
+#
+SUNRISE=$(sunwait poll 47.825N 8.649E)
+#SUNRISE=$(echo "$SUNRISE" | cut -b1-2):$(echo "$SUNRISE" | cut -b3-4):00
+#SUNSET=$(sunwait -p 47N 9E 2>&1 | grep 'Sun rises' | cut -d' ' -f 25)
+#SUNSET=$(echo "$SUNSET" | cut -b1-2):$(echo "$SUNSET" | cut -b3-4):00
+#echo "$SUNRISE"
+DATENOW="$(date +%H:%M:%S)"
+#echo "$DATENOW"
+if [[ "$SUNRISE" = "DAY" ]]; then
+   # gphoto2 --set-config /main/capturesettings/exposurecompensation=15
+echo Normalbelichtung
+else
+    #gphoto2 --set-config /main/capturesettings/exposurecompensation=30
+echo überbelichtung
+fi
+sleep 10
+#echo Klick
+gphoto2 --capture-image-and-download --filename "/var/www/html/webcam.jpg" --force-overwrite
